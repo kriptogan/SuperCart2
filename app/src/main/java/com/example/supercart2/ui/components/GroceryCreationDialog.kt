6d87a78856bfd3dafcefb244bb5555bdd00202da
daniel <daniel@provisionisr.com>
2026-02-02 10:17:23 +0200

    scroll grocery item window

--- app/src/main/java/com/example/supercart2/ui/components/GroceryCreationDialog.kt
+++ app/src/main/java/com/example/supercart2/ui/components/GroceryCreationDialog.kt
@@ -14,6 +14,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -244,7 +246,9 @@ fun GroceryCreationDialog(
         },
         text = {
             Column(
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .verticalScroll(rememberScrollState())
             ) {
                 // Image Section
                 Card(
